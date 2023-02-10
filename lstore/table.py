c04b21c40a0bfe2550eee12f95c100718592d4ae
Tuomas <trickansrud@ucdavis.edu>
2023-02-09 19:36:40 -0800

    added index reminder

--- lstore/table.py
+++ lstore/table.py
@@ -50,6 +50,7 @@ class Table:
         self.bpwrite([*values, schema, None])
         locations = [self.bp, self.bp[0].num_records]
         self.addpd(self.create_rid(), locations)
+        # Add index insert once functional
         return True
 
     def create_rid(self):

b388c6abc72b8725cdc4c3389c3f83aa9371377d
Tuomas <trickansrud@ucdavis.edu>
2023-02-09 19:33:59 -0800

    added intial page implementation

--- lstore/page.py
+++ lstore/page.py
@@ -6,8 +6,14 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        return True
+        try:
+            self.data.index(0)
+            return True
+        except:
+            return False
 
     def write(self, value):
         self.num_records += 1
+        address = self.data.index(0)
+        self.data[address] = value
 

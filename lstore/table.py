f98ba3ac63e2f9d3016ad788e63a37f7b31ba334
santiagomilano <samilano@ucdavis.edu>
2023-02-26 13:29:26 -0800

    BufferPool

--- lstore/table.py
+++ lstore/table.py
@@ -25,11 +25,13 @@ class PageGrp:
         self.id = id
         self.isDirty = False
         self.isPinned = False
+        self.pin_count = 0
         self.rel_path = rel_path+"/pg_"+self.id+".txt"
         self.pages = self.read_from_file()
 
     def pg_write(self, values):
         self.isPinned = True
+        pin_count += 1
         self.isDirty = True
         for i in range(len(self.pages)):
             self.pages[i].write(values[i])
@@ -37,6 +39,7 @@ class PageGrp:
 
     def get_col_values(self, record_no, projected_columns_index):
         self.isPinned = True
+        pin_count += 1
         values = []
         for i in range(0, len(projected_columns_index)):
             if projected_columns_index[i] == 1:
@@ -48,6 +51,7 @@ class PageGrp:
 
     def write_to_file(self):
         self.isPinned = True
+        pin_count += 1
         with open(self.rel_path, "wb") as file:
             for page in self.pages:
                 file.write(page.getAll())
@@ -55,6 +59,7 @@ class PageGrp:
 
     def read_from_file(self):
         self.isPinned = True
+        pin_count += 1
         page_list = []
         with open(self.rel_path, "rb") as file:
             byte = file.read(4096)
@@ -65,22 +70,44 @@ class PageGrp:
 
 
 class BufferPool:
-    def __init__(self, path, num_col):
+    def init(self, path, num_col):
         self.files_in_mem = 0
         self.next_to_eject = None
         self.pages_in_mem = {}  # key: id, Value: PageGrp object
         self.path = path
         self.num_col = num_col
 
-    def add_page(self, id):
-        # assuming buffer pool has space
-        self.pages_in_mem[id] = PageGrp(id, self.path, self.num_col)
-        self.files_in_mem += 1  # should max out at 16
+    def rem_page(self, id):
+        for key in self.pages_in_mem:
+            if self.pages_in_mem[key].isPinned == False:
+                lowest = self.pages_in_mem[key].pin_count
+                break
 
-    def rem_page(self):
+        for key in self.pages_in_mem:
+            if self.pages_in_mem[key].pin_count < lowest and self.pages_in_mem[key].isPinned == False:
+                lowest = key
+
+        if self.pages_in_mem[lowest].isDirty == True:
+            self.pages_in_mem[lowest].write_to_file()
+
+
+        # 1) Remove lowest from dictionary 
+        # 2) Delete the object itself 
+        # 3) Create a new pagegroup object w/ id, path, num_cols arguments
+        # 4) Read from file 
+        # 5) Insert into dictionary
+
         # to be called in add page if buffer pool is full
         # logic to eject a page
-        pass
+
+    def add_page(self, id):
+        if self.files_in_mem == 16:
+            self.rem_page(self, id)
+
+        # assuming buffer pool has space
+        self.pages_in_mem[id] = PageGrp(id, self.path, self.num_col)
+
+        self.files_in_mem += 1  # should max out at 16
 
 
 class Table:

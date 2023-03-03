f935bf1859c127c2cb3632265cfe1855e62431d8
santiagomilano <samilano@ucdavis.edu>
2023-03-02 16:59:46 -0800

    db

--- lstore/db.py
+++ lstore/db.py
@@ -1,11 +1,13 @@
 from lstore.table import Table
 import os
+import json
 
 
 class Database():
 
     def __init__(self):
         self.tables = {}
+        self.table_names = []
         self.path = ""
         pass
 
@@ -18,9 +20,11 @@ class Database():
         if os.path.exists(self.path):
             os.chdir(self.path)
             with open("database.txt", "r") as file:
-                contents = file.read()
-                key, value = contents.replace('{', '').replace('}', '').replace("'", '').split(':')
-                print(key, value)
+                self.table_names = json.loads(file.read())
+                print(self.table_names)
+                #contents = file.read()
+                #key, value = contents.replace('{', '').replace('}', '').replace("'", '').split(':')
+                #print(key, value)
         else:
             os.mkdir(self.path)
             os.chdir(self.path)
@@ -28,8 +32,27 @@ class Database():
 
     def close(self):
         print(self.tables)
+        #write database metadata to file (table names)
         with open("database.txt", "w") as file:
-            file.write(str(self.tables))
+            file.write(json.dumps(self.table_names))
+            print(json.dumps(self.table_names))
+
+        #write table metadata to file (all table attributes)
+        for table in self.tables.values():
+            table_attributes = []
+            table_attributes.append(table.name)
+            table_attributes.append(table.key)
+            table_attributes.append(table.num_columns)
+            table_attributes.append(table.page_directory)
+            table_attributes.append(table.nums)
+            table_attributes.append(table.page_num)
+            table_attributes.append(table.page_range_map)
+            table_attributes.append(table.bp_num)
+            table_attributes.append(table.tp_num)
+            print(table_attributes)
+            with open(table.name+".txt", "w") as file:
+                file.write(table_attributes) 
+
         pass
 
     """
@@ -41,17 +64,17 @@ class Database():
 
     def create_table(self, name, num_columns, key_index):
         # pass path
-        table = Table(name, num_columns, key_index)
 
-        # cwd = os.getcwd()
-        # table_path = os.path.join(cwd, name)
-        # if os.path.exists(table_path):
-        #     print("Table already exists.")
-        #     return self.get_table(name)
-        # else:
-        #     table = Table(name, num_columns, key_index)
-        #     self.tables[name] = table
-        #     os.mkdir(table_path)
+        cwd = os.getcwd()
+        table_path = os.path.join(cwd, name)
+        if os.path.exists(table_path):
+            print("Table already exists.")
+            return self.get_table(name)
+        else:
+            table = Table(name, num_columns, key_index)
+            self.tables[name] = table
+            self.table_names.append(name)
+            os.mkdir(table_path)
         return table
 
     """

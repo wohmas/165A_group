7965af3074f1c485bd8f861b4ce3a60da533e295
Harshil2107 <harshilp2107@gmail.com>
2023-03-12 15:59:12 -0700

    transasction worker skeleton done

--- lstore/lock_manager.py
+++ lstore/lock_manager.py
@@ -5,73 +5,91 @@ import math
 from lstore.transaction import Transaction
 from lstore.table import Table
 
+
 class Lock_Manager:
     def __init__(self):
         self.transactionLockMap = {}
-    
-    def getLock(self, transaction, lock):
+
+    def getLock(self, transaction, lock, type):
         if transaction not in self.transactionLockMap.keys():
             self.transactionLockMap[transaction] = []
+        gotLock = False
+
+        if type == "s":
+            gotLock = lock.getSharedLock(transaction)
+        elif type == "e":
+            gotLock = lock.getExclusiveLock(transaction)
+
+        if not gotLock:
+            return False
+
         self.transactionLockMap[transaction].append(lock)
         return True
-    
+
     def releaseLock(self, transaction, lock):
-        if lock.releaseLock():
+        if lock.releaseLock(transaction):
             self.transactionLockMap[transaction].remove(lock)
             return True
         return False
-        
+
 
 class TPLock:
     def __init__(self):
         self.reading_count = 0
         self.Writing_count = 0
+        self.reading_transactions = []
+        self.writing_transaction = None
+
         self.lock = threading.Lock()
         self.rlock = threading.RLock()
-        self.isLocked = 0 # 0 for not locked, 1 for shread, 2 for exclusive
-        
-    def getSharedLock(self):
+        self.isLocked = 0  # 0 for not locked, 1 for shread, 2 for exclusive
+
+    def getSharedLock(self, transaction):
         self.rlock.acquire()
-        if self.Writing_count == 0:
-            self.isLocked=1
-            self.reading_count+=1
+        if self.Writing_count == 0 or (self.writing_transaction == transaction):
+            self.isLocked = 1
+            self.reading_count += 1
+            self.reading_transactions.append(transaction)
             self.rlock.release()
             return True
         else:
             self.rlock.release()
             return False
-    
-    def getExclusiveLock(self):
+
+    def getExclusiveLock(self, transaction):
         self.lock.acquire()
-        if self.Writing_count == 0 and self.reading_count == 0:
-            self.isLocked=2
+        if self.Writing_count == 0 and (self.reading_count == 0 or (self.reading_count == 1 and self.reading_transactions[0] == transaction)):
+            self.isLocked = 2
             self.Writing_count += 1
+            self.writing_transaction = transaction
             self.lock.release()
             return True
         else:
             self.lock.release()
             return False
-        
-    def __releaseSharedLock(self):
+
+    def __releaseSharedLock(self, transaction):
         self.rlock.acquire()
-        self.reading_count-=1
+        self.reading_transactions.remove(transaction)
+        self.reading_count -= 1
         if self.reading_count == 0:
-            self.isLocked=0
+            self.isLocked = 0
         self.rlock.release()
         return True
-        
+
     def __releaseExclusiveLock(self):
         self.lock.acquire()
-        self.isLocked=0
+        self.isLocked = 0
+        self.writing_transaction = None
         self.Writing_count -= 1
         self.lock.release()
         return True
-    
-    def releaseLock(self):
-        if self.isLocked == 0:
+
+    def releaseLock(self, transaction):
+        if transaction not in self.reading_transactions and transaction != self.writing_transaction:
             return False
-        if self.isLocked == 1:
-            self.__releaseSharedLock()
-            return True
-        self.__releaseExclusiveLock()
-        return True
+        if transaction in self.reading_transactions:
+            self.__releaseSharedLock(transaction)
+        if transaction == self.writing_transaction:
+            self.__releaseExclusiveLock()
+        return False
